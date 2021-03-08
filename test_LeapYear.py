ad2ec1f605743e1c51efe1e2eeda48814dfe2553
kaufmjoh <kaufmjoh@oregonstate.edu>
2021-03-07 18:19:01 -0800

    Wrote the test for years that are multiples of 100. The test failed because there is no implementation.

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -16,5 +16,13 @@ class TestCaseLeapYear(unittest.TestCase):
 		self.assertEqual(LeapYear.LeapYear(2000), "Yes a Leap Year");
 		self.assertEqual(LeapYear.LeapYear(2400), "Yes a Leap Year");
 
+
+	#Test years that are divisible by 100
+	def test100MultLeapYears(self):
+		self.assertEqual(LeapYear.LeapYear(100), "Not a Leap Year");
+		self.assertEqual(LeapYear.LeapYear(1900), "Not a Leap Year");
+		self.assertEqual(LeapYear.LeapYear(2100), "Not a Leap Year");
+
+
 if __name__ == '__main__':
 	unittest.main();
